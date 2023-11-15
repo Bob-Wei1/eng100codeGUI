ee44153d8a97223c1c873e8b5350a16bcfecac69
Jiachen Wei <143958739+Bob-Wei1@users.noreply.github.com>
2023-11-15 16:00:25 -0500

    L

---
+++ resources.cpp
@@ -0,0 +1,5 @@
+//
+// Created by razerblade14 on 11/15/2023.
+//
+
+#include "resources.h"
---
+++ stdafx.h
@@ -0,0 +1,8 @@
+//
+// Created by razerblade14 on 11/15/2023.
+//
+
+#ifndef ENG100CODEGUI_STDAFX_H
+#define ENG100CODEGUI_STDAFX_H
+
+#endif //ENG100CODEGUI_STDAFX_H
